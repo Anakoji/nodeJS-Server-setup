85124ea6afdac5e7f3c8aff5a097777283608728
Jaskaran Brant <jaskaranbrant@gmail.com>
2024-05-10 18:17:10 -0400

    Server setup nodejs

---
+++ index.js
@@ -0,0 +1,15 @@
+// import { getPostsLength } from "./postController";
+import  getPosts, {getPostsLength}  from "./postController.js";
+
+console.log(getPosts());
+
+console.log(`Posts Length: ${getPostsLength()}`);
+// console.log("Hello World!");
+// console.log(global);
+// console.log(process);
+
+// const {generateRandomNumber, celciusToFahrenheit } = require('./utils');
+
+// console.log(`Random number: ${generateRandomNumber()}`);
+
+// console.log(`Celcius to fahrenheit: ${celciusToFahrenheit(0)}`);
